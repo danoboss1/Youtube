c8e938b75367d0f9fd88985abedff2c2c627fb17
danoboss1 <danosehnoutek159@gmail.com>
2023-08-19 13:58:31 +0200

    upravene cvicenia aj so zadanim

--- OOP_1.py
+++ OOP_1.py
@@ -1,18 +1,3 @@
-# Create a Person class with the following attributes and methods:
-
-# Attributes:
-
-# first_name
-# last_name
-# age
-# email
-# Methods:
-
-# __init__(self, first_name, last_name, age, email): Constructor method that initializes the attributes.
-# full_name(self): Method that returns the full name by combining the first name and last name.
-# greet(self): Method that prints a greeting message including the person's full name.
-# get_contact_info(self): Method that returns a dictionary containing the person's full name, age, and email.
-
 class Person:
     def __init__(self, first_name, last_name, age, email):
         self.first_name = first_name
